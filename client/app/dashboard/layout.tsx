22349e2a490219f27d8198be7ace8591b7a5931f
Emmanuel <en56434@gmail.com>
2025-10-23 10:16:02 +0100

    folder organization

--- client/app/dashboard/layout.tsx
+++ client/app/dashboard/layout.tsx
@@ -1,8 +1,15 @@
+import { redirect } from "next/navigation";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const isLoggedIn = false
+
+  if(!isLoggedIn){
+    redirect("/signup")
+  }
   return (
       <main>
         {children}
