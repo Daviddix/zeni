80361eeb92bd0bcb198781122d1f85af463aeda1
Emmanuel <en56434@gmail.com>
2025-10-23 09:59:46 +0100

    added heading switch functionality to auth headings

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,39 @@
+"use client"
 import Image from 'next/image';
 import "./auth.global.css"
 import logo from "@/public/images/logo.svg"
+import { usePathname } from 'next/navigation';
+
 
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const authHeadings = [
+    {
+      match : "/login",
+      toRender : `Welcome Back. <br />
+       Login to continue using Zeni`
+    },
+    {
+      match : "/signup",
+      toRender : `Welcome Aboard. <br />
+Create an account to start using Zeni`
+    },
+    {
+      match : "/currency",
+      toRender : `Select your currency`
+    },
+    {
+      match : "/signup/info",
+      toRender : `You're almost there`
+    }
+  ]
+
+  const currentPathName = usePathname()
+  const textToRender = authHeadings.find((obj)=> obj.match == currentPathName)?.toRender as (string | TrustedHTML)
+
   return (
       <main className='auth-main'>
         <header>
@@ -15,10 +42,8 @@ export default function AuthLayout({
             alt={"logo"}
             />
 
-            <h1 className="auth-heading">
-                Welcome Aboard.
-                <br />
-                Create an account to start using Zeni
+            <h1 dangerouslySetInnerHTML={{__html : textToRender}} className="auth-heading">
+               
             </h1>
         </header>
         {children}
